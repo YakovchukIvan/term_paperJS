a9d574df323fa2eba87936bc482a2c5c753ddd6a
litvinovjobs@gmail.com <litvinovjobs@gmail.com>
2023-04-15 20:28:58 +0300

    working codding update

--- script/script.js
+++ script/script.js
@@ -2,11 +2,14 @@
 
 const inputValid = document.querySelector(".inputValid") // inputValid
 const btnSearch = document.querySelector(".btnSearch") // кнопка пошуку міста
-const list__city = document.querySelector(".list__city") // список погоди
+// const inputCity = document.querySelector(".inputCity") // input search
 const title__city = document.querySelector(".title__city") // h1 
 const tr__title = document.querySelector(".tr__title") // години погоди
+const inputCity = document.getElementById("inputCity")
 
+const table_list = document.querySelector(".table_list")
 const table__title = document.querySelector(".table__title")
+const tr__icon = document.querySelector(".tr__icon")
 const temp = document.querySelector(".temp")
 const temp__min = document.querySelector(".temp__min")
 const temp__max = document.querySelector(".temp__max")
@@ -31,10 +34,9 @@ formCity.addEventListener("submit", (event) => {
     const urlCity = `https://raw.githubusercontent.com/Adushar/UkraineCitiesAndVillages/main/CitiesAndVillages%20-%2014%20March.json`
 
     fetchWeather(url)
-
+    
     // ukraine(urlCity)
 })
-
 // async function ukraine(urlCity){
 //     const res = await fetch(urlCity);
 //     console.log(res);
@@ -45,13 +47,15 @@ formCity.addEventListener("submit", (event) => {
 
 
 async function fetchWeather(url){
+    table_list.style.display = "block"
     const res = await fetch(url);
     inputValid.style.display = "none";
     if (res.status === 404) { // валідатор помилки 404
         console.log('помилка');
         inputValid.style.display = "block";
     }
-    
+
+    inputCity.value = "" // очищуємо пошук міста
     console.log(res);
     data = await res.json();
     console.log('data: ', data);
@@ -61,9 +65,6 @@ async function fetchWeather(url){
 
 function startList() {
 
-
-
-
     const cityList = data["list"];
     console.log('rivne: ', cityList);
     const dataCity1 = cityList[0];
@@ -71,36 +72,17 @@ function startList() {
     console.log('dataCity1: ', dataCity1);
     console.log(dataCity1.weather[0]);
     console.log(dataCity1.weather[0]["description"]);
-    list__city.innerHTML = "" // очищуємо список коли вводимо місто ще раз
+
     title__city.innerHTML = `<span>${data.city["name"]}</span> <span>${dataCity1.weather[0]["description"]}</span> <img src='http://openweathermap.org/img/wn/${dataCity1.weather[0].icon}@2x.png' alt="">`
-    // console.clear();
-
-    tableHours(); // запускаємо функцію яка вставляє години погоди
-
-    list__city.insertAdjacentHTML("beforeend", 
-    `
-    <li><span>Місто</span> ${data.city["name"]}</li>
-    <li><span>Погода : </span> ${dataCity1.weather[0]["description"]} <img src='http://openweathermap.org/img/wn/${dataCity1.weather[0].icon}@2x.png' alt=""></li>
-    <li><span>Дата погоди: </span> ${dataCity1["dt_txt"]}</li>
-    <li><span>Вологість : </span> ${dataCity1.main["humidity"]} % </li>
-    <li><span>Відчувається як : </span> ${dataCity1.main["feels_like"]} °C </li>
-    <li><span>Максимальна температура повітря : </span> ${dataCity1.main["temp"]} °C </li>
-    <li><span>Мінімальна температура повітря : </span> ${dataCity1.main["temp_min"]} °C </li>
-    <li><span>Тиск в повітрі : </span> ${dataCity1.main["pressure"]} Па</li>
-    <li><span>Тиск на рівня моря : </span> ${dataCity1.main["sea_level"]} мм рт. </li>
-    <li><span>Порив вітру : </span> ${dataCity1.wind["gust"]} км </li>
-    <li><span>Швидкість вітру : </span> ${dataCity1.wind["speed"]} км </li>
-    
-    `
-    )
+    tableStart(); // запускаємо функцію яка заповнює таблицю
 }
 
-function tableHours() { // функція яка вставляє години погоди
-    
-const tdList = document.querySelectorAll('td'); // отримати список всіх тегів <td>
-for (let i = 4; i < tdList.length; i++) {
-    tdList[i].remove(); // видалити поточний тег <td>
-}
+function tableStart() { // функція яка вставляє години погоди
+
+    const tdList = document.querySelectorAll('td'); // отримати список всіх тегів <td>
+    for (let i = 4; i < tdList.length; i++) {
+        tdList[i].remove(); // видалити поточний тег <td>, очищує таблицю коли вводимо нове місто
+    }
 
     for (let i = 0; i < 8; i++) {
         const dateTimeString = data["list"][i]["dt_txt"];
@@ -119,18 +101,32 @@ for (let i = 4; i < tdList.length; i++) {
 
     for (let i = 0; i < 8; i++) {
         const city = data["list"][i];
-        const result = city.main["temp"];
+        console.log(city);
+        const result = city.weather[0].icon;
+        const description = city.weather[0].description;
+        console.log(result);
+        tr__icon.insertAdjacentHTML("beforeend", 
+        `
+        <td><img src='http://openweathermap.org/img/wn/${result}@2x.png' alt="result" title="${description}"></td>
+        `
+        )
+        // цикл для icon
+    } 
+
+    for (let i = 0; i < 8; i++) {
+        const city = data["list"][i];
+        const result = Math.round(city.main["temp"]);
         temp.insertAdjacentHTML("beforeend", 
         `
         <td>${result} °C</td>
         `
         )
         // цикл для температури 
-    } 
+    }
 
     for (let i = 0; i < 8; i++) {
         const city = data["list"][i];
-        const result = city.main["temp_min"];
+        const result = Math.round(city.main["temp_min"]);
         temp__min.insertAdjacentHTML("beforeend", 
         `
         <td>${result} °C</td>
@@ -141,7 +137,7 @@ for (let i = 4; i < tdList.length; i++) {
 
     for (let i = 0; i < 8; i++) {
         const city = data["list"][i];
-        const result = city.main["temp_max"];
+        const result = Math.round(city.main["temp_max"]);
         temp__max.insertAdjacentHTML("beforeend", 
         `
         <td>${result} °C</td>
@@ -152,7 +148,7 @@ for (let i = 4; i < tdList.length; i++) {
 
     for (let i = 0; i < 8; i++) {
         const city = data["list"][i];
-        const result = city.main["feels_like"];
+        const result = Math.round(city.main["feels_like"]);
         feels__like.insertAdjacentHTML("beforeend", 
         `
         <td>${result} °C</td>
@@ -174,7 +170,7 @@ for (let i = 4; i < tdList.length; i++) {
 
     for (let i = 0; i < 8; i++) {
         const city = data["list"][i];
-        const result = city.wind["gust"];
+        const result = Math.round(city.wind["gust"]);
         gust__wind.insertAdjacentHTML("beforeend", 
         `
         <td>${result} м/сек</td>
@@ -185,7 +181,7 @@ for (let i = 4; i < tdList.length; i++) {
 
     for (let i = 0; i < 8; i++) {
         const city = data["list"][i];
-        const result = city.wind["speed"];
+        const result = Math.round(city.wind["speed"]);
         speed__wind.insertAdjacentHTML("beforeend", 
         `
         <td>${result} м/сек</td>
